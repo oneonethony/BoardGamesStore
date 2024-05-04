6b95eee5fcf3218fa35eb0943067c930bc27a595
oneonethony <anton010197@mail.ru>
2024-05-04 15:24:59 +0400

    init project

---
+++ BoardGamesStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoardGamesStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
